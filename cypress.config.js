0046a0fa3a1b7fd812cfd94eb317f310107fea77
choitim <choitim2010@gmail.com>
2022-11-24 14:17:33 -0600

    with mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Tim report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
+    
     experimentalStudio: true,
     chromeWebSecurity: false,
     experimentalStudio: true,
+    screenshotOnRunFailure : true,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
